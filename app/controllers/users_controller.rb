3c6b131a7970feba3602c3f4a2340f5e713e5fae
Miss-Cheese <nat.galligan@gmail.com>
2022-01-16 19:05:29 -0500

    Fetch users from Star Wars API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
-    
+    def index
+        users = fetch_users
+        render json: users["results"]
+    end
+
+    private
+    def fetch_users
+        url = "https://swapi.dev/api/people"
+        uri = URI(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Get.new(uri.path, {"Content-Type" => "application/json"})
+        response = http.request(request)
+        body = JSON.parse(response.body)
+    end
 end
